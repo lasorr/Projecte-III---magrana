312d875d142421422d36c770fb7372035e6f4900
lasorr <laia.sorribes@estudiantat.upc.edu>
2026-03-11 11:21:57 +0100

    YA ESTA HITO 1

--- Magrana_hito1/Assets/Scripts/Basics Jugadora/Colpejar.cs
+++ Magrana_hito1/Assets/Scripts/Basics Jugadora/Colpejar.cs
@@ -6,28 +6,41 @@ public class Colpejar : MonoBehaviour
     public BoxCollider Jugadora;
     public BoxCollider ObjecteEspecial;
     public GameObject Edifici;
+    public GameObject Monja;
     public Material colorVerd;
     
-    private Renderer rendererObjectiu;
-    private Material colorOriginal;
+        private Renderer rendererEdifici;
+    private Renderer rendererMonja;
 
     void Start()
     {
         if (Edifici != null)
-        {
-            rendererObjectiu = Edifici.GetComponent<Renderer>();
-            if (rendererObjectiu != null)
-                colorOriginal = rendererObjectiu.material;
-        }
+            rendererEdifici = Edifici.GetComponent<Renderer>();
+            
+        if (Monja != null)
+            rendererMonja = Monja.GetComponent<Renderer>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other == ObjecteEspecial)
+        // Si colisiona con ObjecteEspecial, cambiar color del Edifici
+        if (ObjecteEspecial != null && other == ObjecteEspecial)
         {
-            Debug.Log("¡Golpeado!");
-            if (rendererObjectiu != null && colorVerd != null)
-                rendererObjectiu.material = colorVerd;
+            Debug.Log("¡Golpeado ObjecteEspecial!");
+            if (rendererEdifici != null && colorVerd != null)
+                rendererEdifici.material = colorVerd;
+        }
+
+        // Si colisiona con Monja, cambiar color de la Monja
+        if (Monja != null)
+        {
+            Collider colliderMonja = Monja.GetComponent<Collider>();
+            if (other == colliderMonja)
+            {
+                Debug.Log("¡Yassificada!");
+                if (rendererMonja != null && colorVerd != null)
+                    rendererMonja.material = colorVerd;
+            }
         }
     }
 }
