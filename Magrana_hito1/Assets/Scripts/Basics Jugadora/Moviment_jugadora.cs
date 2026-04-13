43c643305beaca9bb4fedab17d7427a7ac8f7f85
lasorr <laia.sorribes@estudiantat.upc.edu>
2026-04-13 19:07:23 +0200

    animator caminar

--- Magrana_hito1/Assets/Scripts/Basics Jugadora/Moviment_jugadora.cs
+++ Magrana_hito1/Assets/Scripts/Basics Jugadora/Moviment_jugadora.cs
@@ -6,11 +6,13 @@ public class Moviment_jugadora : MonoBehaviour
     private Rigidbody rb;
     public float velocitat = 5f;
     public InputActionReference move;
+    public Animator animator;
     
     [SerializeField] private Transform modelTransform; // arrossega el empty object que contengui els objectes player
     [SerializeField] private float rotationSpeed = 8f; // velocitat de gir
     
     private Vector2 moveDirection;
+    private float currentSpeed;
 
     void Start()
     {
@@ -20,7 +22,14 @@ public class Moviment_jugadora : MonoBehaviour
     void Update()
     {
         if (move != null)
+        {
             moveDirection = move.action.ReadValue<Vector2>();
+        }
+
+        Vector3 horizontalVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+        currentSpeed = horizontalVelocity.magnitude / velocitat; // Normalizado entre 0 y 1
+        animator.SetFloat("Moviment", currentSpeed);
+            
     }
 
     private void FixedUpdate()
