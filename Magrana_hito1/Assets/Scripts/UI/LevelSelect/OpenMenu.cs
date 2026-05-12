3ab8ceb98e83813b3171b13fb6fabb6674728613
ovi1310 <ovidiu-ioan.nerges@estudiantat.upc.edu>
2026-05-12 20:49:50 +0200

    Escape obre i tanca el menú

--- Magrana_hito1/Assets/Scripts/UI/LevelSelect/OpenMenu.cs
+++ Magrana_hito1/Assets/Scripts/UI/LevelSelect/OpenMenu.cs
@@ -1,20 +1,53 @@
+
 using UnityEngine;
+using System.Collections;
 
 public class OpenMenu : MonoBehaviour
 {
-    public GameObject pausePanel; // Panell que controlem
+    public GameObject mainMenuPanel; // Panell que controlem
     void Start()
     {
+        Debug.Log("Menu Tancat");
         // Assegurem que el menú està tancat per default
-        if (pausePanel != null)
-            pausePanel.SetActive(false); // Deshabilita el panell del menú
+        if (mainMenuPanel != null)
+            mainMenuPanel.SetActive(false); // Deshabilita el panell del menú
+            Debug.Log("Menu Tancat");
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(mainMenuPanel.activeSelf) // Detectar si el menú ja està obert
+            {
+                CloseMainMenu();
+                Debug.Log("Menu Tancat");
+            }
+            else
+            {
+                OpenMainMenu();
+                Debug.Log("Menu Obert");
+            }
+        }
     }
 
+    /* Important que la interfície que volem que actui com a botó, tingui el component Button
+    A l'apartat OnClick() d'aquest component, establim que MenuManager executi la següent funció */
     public void OpenMainMenu()
     {
-        if (pausePanel != null)
+        if (mainMenuPanel != null)
         {
-            pausePanel.SetActive(true); // Habilita el menú
+            mainMenuPanel.SetActive(true); // Habilita el menú
+            Debug.Log("Menú obert");
+        }
+    }
+
+    public void CloseMainMenu()
+    {
+        if (mainMenuPanel != null)
+        {
+            mainMenuPanel.SetActive(false); // Deshabilita el menú
+            Debug.Log("Menú tancat");
+
         }
     }
 }
