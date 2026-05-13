56ce2362d1ba0733024cb5c5dae6fbb19e1c7498
ovi1310 <ovidiu-ioan.nerges@estudiantat.upc.edu>
2026-05-13 13:25:34 +0200

    Menu principal ja es funcional

--- Magrana_hito1/Assets/Scripts/UI/LevelSelect/MenuManager.cs
+++ Magrana_hito1/Assets/Scripts/UI/LevelSelect/MenuManager.cs
@@ -6,12 +6,19 @@ public class MenuManager : MonoBehaviour
     public GameObject mainMenuPanel; // Panell que controlem
     public GameObject mainMenuUI; // Botons del manú principal
     public GameObject optionsUI; // Botons de les opcions
+    public GameObject controlsUI; // Esquema dels controls
+    public GameObject quitConfirmUI; // Missatge de confirmació sortir
+
     void Start()
     {
         Debug.Log("Menu Tancat");
         // Assegurem que el menú està tancat per default
         if (mainMenuPanel != null)
+        {
             mainMenuPanel.SetActive(false); // Deshabilita el panell del menú
+            optionsUI.SetActive(false);
+            controlsUI.SetActive(false);
+        }
     }
     void Update()
     {
@@ -21,11 +28,13 @@ public class MenuManager : MonoBehaviour
             {
                 CloseMainMenu();
                 HideOptions(); // Si les opcions estan obertes, al tornar a obrir el menú no cal que tornin a obrir-se
+                HideControls();
                 Debug.Log("Menu Tancat");
             }
             else
             {
                 OpenMainMenu();
+                mainMenuPanel.SetActive(true);
                 Debug.Log("Menu Obert");
             }
         }
@@ -38,10 +47,11 @@ public class MenuManager : MonoBehaviour
         if (mainMenuPanel != null)
         {
             mainMenuPanel.SetActive(true); // Habilita el menú
+            optionsUI.SetActive(false);
+            controlsUI.SetActive(false);
             Debug.Log("Menú obert");
         }
     }
-
     public void CloseMainMenu()
     {
         if (mainMenuPanel != null)
@@ -64,5 +74,41 @@ public class MenuManager : MonoBehaviour
         mainMenuUI.SetActive(true);
         optionsUI.SetActive(false);
         Debug.Log("Opcions tancades, menú principal obert");
+    }
+
+    public void ShowControls()
+    {
+        mainMenuUI.SetActive(false);
+        controlsUI.SetActive(true);
+
+        Debug.Log("Controls oberts");
+    }
+    public void HideControls()
+    {
+        mainMenuUI.SetActive(true);
+        controlsUI.SetActive(false);
+
+        Debug.Log("Controls tancats, menú principal obert");
+    }
+
+    public void ShowQuitConfirm()
+    {
+        quitConfirmUI.SetActive(true);
+
+        Debug.Log("Vols sortir?");
+    }
+    public void HideQuitConfirm()
+    {
+        quitConfirmUI.SetActive(false);
+
+        Debug.Log("No vol sortir");
+    }
+    public void QuitConfirmed()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;  // Para pruebas en el Editor
+        #else
+        Application.Quit();  // Para la build final
+        #endif
     }
 }
