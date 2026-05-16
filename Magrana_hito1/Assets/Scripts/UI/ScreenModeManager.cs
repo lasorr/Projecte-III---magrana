b9c5f62d7eb8843f8352a251eccc4e3f4169be9c
ovi1310 <ovidiu-ioan.nerges@estudiantat.upc.edu>
2026-05-16 18:59:21 +0200

    si

--- Magrana_hito1/Assets/Scripts/UI/ScreenModeManager.cs
+++ Magrana_hito1/Assets/Scripts/UI/ScreenModeManager.cs
@@ -5,6 +5,8 @@ public class ScreenModeManager : MonoBehaviour
 {
     public Button fullScreenButton;
     public Button windowedButton;
+    public Color selectedColor = new Color(242,238,139);
+    public Color colorNoSeleccionado = Color.white;
     void Start()
     { /* Afegim un listener que estigui pendent de quin botó es selecciona */
         fullScreenButton.onClick.AddListener(() => Screen.fullScreen = true);
