c55bb260695c67f6cd6ab4da09d0f995736c2914
ovi1310 <ovidiu-ioan.nerges@estudiantat.upc.edu>
2026-06-01 20:00:13 +0200

    referencies sons

--- Magrana_hito1/Assets/Scripts/Audio/FXsManager.cs
+++ Magrana_hito1/Assets/Scripts/Audio/FXsManager.cs
@@ -2,7 +2,40 @@ using UnityEngine;
 
 public class FXsManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public AudioSource sfxSource;
+ #region AUDIO CLIPS
+    [Header("UI")]
+    public AudioClip clickClip;
+    public AudioClip focusedClip;
+    public AudioClip initialCountdownClip;
+    public AudioClip finalCountdownClip;
+    public AudioClip endAlarmClip;
+
+    [Header("Gameplay")]
+    public AudioClip spawnClip;
+    public AudioClip walkClip;
+    public AudioClip wooshClip;
+    public AudioClip stunnedClip;
+    public AudioClip powerUpClip;
+    public AudioClip powerDownClip;
+    public AudioClip transformClip;
+    public AudioClip unTransformClip;
+    public AudioClip winnerClip;
+
+    [Header("Characters")]
+    public AudioClip porcliciesClip;
+    public AudioClip[] mussolClips;
+    public AudioClip[] monjaClips;
+    public AudioClip[] iaiaClips;
+    public AudioClip dragQueenClip;
+
+    [Header("Environment")]
+    public AudioClip officeClip;
+    public AudioClip coffeeShopClip;
+    public AudioClip desnonamentClip;
+    public AudioClip edificiCatolicClip;
+    #endregion
+
     void Start()
     {
         
--- Magrana_hito1/Assets/Scripts/Audio/MusicManager.cs
+++ Magrana_hito1/Assets/Scripts/Audio/MusicManager.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public AudioSource musicSource;
+    public AudioClip musicClip;
+    private static MusicManager instance;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if (instance == null) // Singleton: evita duplicats d'audio manager
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject); // Destruye el duplicado si ya existe uno
+        }
+    }
+    private void Start()
+    {
+        if (musicSource != null)
+        {
+            musicSource.clip = musicClip;
+            musicSource.Play();
+        }
     }
 }
