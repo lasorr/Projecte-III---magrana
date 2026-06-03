a6a78079566275fc4596ef62cc50c48cdf664df2
martina <martina.bo@estudiantat.upc.edu>
2026-06-03 22:57:29 +0200

    ui funciona menu pausa !!!!
    nomes falta lo de "tornar" q com no està l'escena de selecció nivell no l'he pogut enllaçar

--- Magrana_hito1/Assets/Scripts/UI/Menu Pausa/MenuOpcions.cs
+++ Magrana_hito1/Assets/Scripts/UI/Menu Pausa/MenuOpcions.cs
@@ -3,9 +3,6 @@ using UnityEngine.UI;
 
 public class MenuOpcions : MonoBehaviour
 {
-    public Slider sliderMusica;
-    public Slider sliderSFX;
-
     public void PantallaCompleta()
     {
         Screen.fullScreen = true;
@@ -14,15 +11,5 @@ public class MenuOpcions : MonoBehaviour
     public void Finestra()
     {
         Screen.fullScreen = false;
-    }
-
-    public void VolumMusica()
-    {
-        AudioListener.volume = sliderMusica.value;
-    }
-
-    public void VolumSFX()
-    {
-        Debug.Log("SFX Volume: " + sliderSFX.value);
     }
 }
