ab2a3073cf5aea5b50c99c1f5afe0d07a8c18f82
martina <martina.bo@estudiantat.upc.edu>
2026-06-03 23:41:33 +0200

    ara si arreglao

--- Magrana_hito1/Assets/Scripts/UI/SeleccioNivells.cs
+++ Magrana_hito1/Assets/Scripts/UI/SeleccioNivells.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 
 public class SeleccioNivells : MonoBehaviour
 {
+    public float alphaThreshold = 0.1f;
     void Start()
     {
         this.GetComponent<Image>().alphaHitTestMinimumThreshold = alphaThreshold;
